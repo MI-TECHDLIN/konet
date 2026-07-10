ffc856b61baa0edf7541d77a29c74d31f235120f
MI-TECHDLIN <ezechukwumiracle52@gmail.com>
2026-07-10 12:55:40 +0100

    style: inbox card and pinned widgte

---
+++ lib/screens/messaging/widget/pineed_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class pinned_widget extends StatelessWidget {
+  pinned_widget({
+    super.key,
+    required this.text,
+    required this.grad1,
+    required this.grad2,
+  });
+
+  String text;
+  int grad1;
+  int grad2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(0, 14, 22, 0),
+
+      child: Column(
+        children: [
+          Container(
+            height: 60,
+            width: 60,
+
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(9999),
+              gradient: LinearGradient(colors: [Color(grad1), Color(grad2)]),
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            text,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontWeight: FontWeight.w600, color: Colors.black),
+          ),
+        ],
+      ),
+    );
+  }
+}
