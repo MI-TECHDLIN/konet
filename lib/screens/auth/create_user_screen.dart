6f79a7b3d90cd11b956ba9f0d09b9f5f7121f861
MI-TECHDLIN <ezechukwumiracle52@gmail.com>
2026-07-23 21:16:11 +0100

    feat: error catch for firebase auth

--- lib/screens/auth/create_user_screen.dart
+++ lib/screens/auth/create_user_screen.dart
@@ -33,9 +33,31 @@ for auth
       );
       print('${newUser.user!.email}. User created');
     } on FirebaseAuthException catch (e) {
-      ScaffoldMessenger(child: SnackBar(content: Text(e.message.toString())));
+       ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              dismissDirection: DismissDirection.up,
+              backgroundColor: Colors.red,
+              behavior: SnackBarBehavior.floating,
+              margin: EdgeInsets.only(
+                bottom: MediaQuery.of(context).size.height - 120,
+                left: 16,
+                right: 16,
+              ),
+              content: Text(e.toString());
+            ),
     } catch (e) {
-      ScaffoldMessenger(child: SnackBar(content: Text(e.toString())));
+     ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              dismissDirection: DismissDirection.up,
+              backgroundColor: Colors.red,
+              behavior: SnackBarBehavior.floating,
+              margin: EdgeInsets.only(
+                bottom: MediaQuery.of(context).size.height - 120,
+                left: 16,
+                right: 16,
+              ),
+              content: Text(e.toString()),
+            ),
     }
   }
 
